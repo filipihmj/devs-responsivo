a192c5aeb8e5e4898096afd287116232257e1525
isabele rau <isabele.rau@softplan.com.br>
2018-09-19 14:49:43 -0300

    teste swipe with hammerjs

---
+++ swipe.js
@@ -0,0 +1,10 @@
+var myElement = document.getElementById('myElement');
+
+// create a simple instance
+// by default, it only adds horizontal recognizers
+var mc = new Hammer(myElement);
+
+// listen to events...
+mc.on(function(ev) {
+    myElement.textContent = ev.type;
+});
