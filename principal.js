f17862a930d60b3e814cec7b5bfa86cb05e158ad
isabele rau <isabele.rau@softplan.com.br>
2018-09-20 12:50:37 -0300

    add js ontouch

--- principal.js
+++ principal.js
@@ -1,19 +1,5 @@
-
-
-$('.owl-carousel').owlCarousel({
-    rtl:true,
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-});
+function myFunction(event) {
+    var x = event.touches[0].clientX;
+    var y = event.touches[0].clientY;
+    document.getElementById(".demo").innerHTML = x + ", " + y;
+}
