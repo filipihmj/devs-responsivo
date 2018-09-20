3918d3bd62935595cf7cadac1e27f5e18565283b
Filipi Julio <filipihmj@gmail.com>
2018-09-20 13:23:08 -0300

    mudancas owl

--- principal.js
+++ principal.js
@@ -1,5 +1,24 @@
 function myFunction(event) {
     var x = event.touches[0].clientX;
     var y = event.touches[0].clientY;
-    document.getElementById(".demo").innerHTML = x + ", " + y;
+    document.getElementById(".demo").innerHTML = x + y;
 }
+
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
