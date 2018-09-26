8d690f2a7c2028e02644061b035d3ab2413d5b6d
Filipi Julio <filipihmj@gmail.com>
2018-09-26 11:55:57 -0300

    Ultimas alterações

--- js/carousel.js
+++ js/carousel.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
     $(".owl-touch").owlCarousel({
         navigation : true,
         loop : true,
-        slideSpeed : 300,
-        paginationSpeed : 400,
+        center: true,
         singleItem : true,
         items: 1,
 
@@ -11,8 +10,27 @@ $(document).ready(function(){
             768: {
                 items: 2,
             },
-            1200: {
+            1030: {
                 items: 3,
+            }
+        }
+    });
+});
+
+$(document).ready(function(){
+    $("#owl-carousel-galeria").owlCarousel({
+        navigation : true,
+        loop : true,
+        center: true,
+        singleItem : true,
+        items: 1,
+
+        responsive: {
+            768: {
+                items: 1,
+            },
+            1030: {
+                items: 2,
             }
         }
     });
